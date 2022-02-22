b676a597413922b1136975373a1823f4155870b3
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-02-22 14:35:52 -0500

    fixed tablebase promotion

--- syzygy.c
+++ syzygy.c
@@ -7,7 +7,7 @@
 #include "Fathom/tbprobe.h"
 
 
-int parse_tb_move(unsigned from, unsigned to, Board *board){
+int parse_tb_move(unsigned from, unsigned to, unsigned prom, Board *board){
 
     MoveList legalMoves;
     memset(&legalMoves, 0, sizeof legalMoves);
@@ -15,6 +15,14 @@ int parse_tb_move(unsigned from, unsigned to, Board *board){
 
     for (int i = 0; i < legalMoves.count; i++) {
         if ((getsource(legalMoves.moves[i]) == from) && (gettarget(legalMoves.moves[i]) == to)){
+            if ((prom == TB_PROMOTES_QUEEN) && (getpromoted(legalMoves.moves[i]) != Q) && (getpromoted(legalMoves.moves[i]) != q))
+                continue;
+            if ((prom == TB_PROMOTES_KNIGHT) && (getpromoted(legalMoves.moves[i]) != N) && (getpromoted(legalMoves.moves[i]) != n))
+                continue;
+            if ((prom == TB_PROMOTES_BISHOP) && (getpromoted(legalMoves.moves[i]) != B) && (getpromoted(legalMoves.moves[i]) != b))
+                continue;
+            if ((prom == TB_PROMOTES_ROOK) && (getpromoted(legalMoves.moves[i]) != R) && (getpromoted(legalMoves.moves[i]) != r))
+                continue;
             return legalMoves.moves[i];
         }
     }
@@ -40,5 +48,5 @@ int get_root_move(Board *board){
                                    (board->bitboards[b] | board->bitboards[B]), (board->bitboards[N] | board->bitboards[n]),
                                    (board->bitboards[P] | board->bitboards[p]), 0, board->castle, 0, (bool)board->side, results);
 
-    return parse_tb_move(TB_GET_FROM(tbres), TB_GET_TO(tbres), board);
+    return parse_tb_move(TB_GET_FROM(tbres), TB_GET_TO(tbres), TB_GET_PROMOTES(tbres), board);
 }
