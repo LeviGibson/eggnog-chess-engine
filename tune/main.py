f6944afc9c49cb3afe631029cfb42846c951df6c
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-03-16 17:15:47 -0400

    reinstated old tuning system

--- tune/main.py
+++ tune/main.py
@@ -1,25 +1,17 @@
 import puzzles
 import matplotlib.pyplot as plt
-import numpy as np
-import multiprocessing
 
-processes = []
 scores = []
 
-testingvalue = 0
-while testingvalue < 5000:
+testingvalue = 10
+while testingvalue < 1000:
     print(testingvalue)
-    processes.append(multiprocessing.Process(target=puzzles.run, args=(testingvalue, 1700)))
-    # scores.append(score)
-    #
-    # plt.plot(scores)
-    # plt.savefig("plot.png")
-    
-    # print(scores)
-    testingvalue += 1000
 
-for id, obj in enumerate(processes):
-    processes[id].start()
+    score = puzzles.run(testingvalue)
+    scores.append(score)
 
-for id, obj in enumerate(processes):
-    processes[id].join()
+    plt.plot(scores)
+    plt.savefig("plot.png")
+
+    print(scores)
+    testingvalue += 10
--- tune/puzzles.py
+++ tune/puzzles.py
@@ -1,87 +1,46 @@
 import chess.engine
-import random
 
 import logging
 logging.basicConfig(level=logging.CRITICAL)
 
-def run(value : int, default : int):
-    dengine = chess.engine.SimpleEngine.popen_uci("../bin/eggnog-chess-enging-avx2-linux")
-    tengine = chess.engine.SimpleEngine.popen_uci("../bin/eggnog-chess-enging-avx2-linux")
+def run(value : int):
 
-    # dengine.configure({"Threads" : 6})
-    dengine.configure({"Tune" : default})
-    dengine.configure({"SyzygyPath" : "/home/levigibson/Documents/static/arenalinux_64bit_3.10beta/TB/syzygy"})
+    #download this large file with `wget https://database.lichess.org/lichess_db_puzzle.csv.bz2`
+    puzzleFile = open("lichess_db_puzzle.csv", 'r')
 
-    # tengine.configure({"Threads": 6})
-    tengine.configure({"Tune": value})
-    tengine.configure({"SyzygyPath": "/home/levigibson/Documents/static/arenalinux_64bit_3.10beta/TB/syzygy"})
-    score = 0
+    engine = chess.engine.SimpleEngine.popen_uci("../bin/eggnog-chess-engine-avx2-linux")
+    engine.configure({"Tune" : value})
 
-    for game in range(20):
-        board = chess.Board()
+    board = chess.Board()
 
-        while not board.is_game_over():
+    total = 0
+    correct = 0
+    index = 0
 
-            result = None
-            if board.turn == chess.WHITE:
-                result = tengine.analyse(board, chess.engine.Limit(time=random.uniform(.4, .6)))
-            else:
-                result = tengine.analyse(board, chess.engine.Limit(time=random.uniform(.4, .6)))
+    while index < 100:
 
-            try:
-                if result['score'].relative.score() > 300:
-                    if board.turn == chess.BLACK: score -= 1; break
-                    elif board.turn == chess.WHITE: score += 1; break
-            except:
-                break
+        command = puzzleFile.readline()
+        # if 'kingsideAttack' not in command:continue
 
-            if result['score'].relative.score() < -300:
-                if board.turn == chess.WHITE: score -= 1; break
-                elif board.turn == chess.BLACK: score += 1; break
+        command = command.split(',')
 
-            board.push(result['pv'][0])
-            if board.is_checkmate():
-                if board.turn == chess.WHITE: score -= 1; break
-                elif board.turn == chess.BLACK: score += 1; break
+        if (int(command[3]) < 2600):
+            continue
 
-            if board.is_stalemate() or board.is_repetition(3) or board.is_fifty_moves():
-                break
-        print(game)
-    print()
-    print(value, score)
-    tengine.quit()
-    dengine.quit()
+        index += 1
 
-    # board = chess.Board()
-    #
-    # total = 0
-    # correct = 0
-    # index = 0
-    #
-    # while index < 100:
-    #
-    #     command = puzzleFile.readline()
-    #     # if 'kingsideAttack' not in command:continue
-    #
-    #     command = command.split(',')
-    #
-    #     if (int(command[3]) < 2600):
-    #         continue
-    #
-    #     index += 1
-    #
-    #     board.set_fen(command[1])
-    #     board.push_uci(command[2].split(' ')[0])
-    #
-    #     result = engine.play(board, chess.engine.Limit(time=.75))
-    #
-    #     total += 1
-    #     if str(result.move) == command[2].split(' ')[1]:
-    #         correct += 1
-    #
-    # engine.quit()
-    #
-    # return correct
+        board.set_fen(command[1])
+        board.push_uci(command[2].split(' ')[0])
 
-# if __name__ == '__main__':
-#     run(2000)
+        result = engine.play(board, chess.engine.Limit(time=.75))
+
+        total += 1
+        if str(result.move) == command[2].split(' ')[1]:
+            correct += 1
+
+    engine.quit()
+
+    return correct
+
+if __name__ == '__main__':
+    run(2000)
