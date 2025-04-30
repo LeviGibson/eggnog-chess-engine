1e78e8d36f937dec00f43af255af3df2b7a36f24
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2025-04-30 14:57:27 -0400

    removed bad code

--- search.c
+++ search.c
@@ -333,11 +333,7 @@ static inline int32_t search(int32_t depth, int32_t alpha, int32_t beta, Line *p
 //        }
 
         //TODO tune this
-#ifndef NO_LMR
-        if (legalMoveCount && depth <= 2 && (!getcapture(move)) && (!in_check) && !board->pvnode && ((staticeval - 13000 + (legalMoves.scores[moveId] / 2)) < alpha) && !is_move_direct_check(move, board))
-            continue;
-
-#endif
+        
         //since king moves are super expensive (for the neural networks) many of them are discarded at depth 1.
         if (legalMoveCount && depth == 1 && !board->pvnode && !in_check && legalMoves.scores[moveId] < -25360 && (getpiece(move) == p_K || getpiece(move) == p_k))
             continue;
